4fbefa4e1a44087725c32c7a9d90f2a7e7177850
shahuji <sonushahuji4@gmail.com>
2020-05-19 18:41:24 +0530

    Julka

---
+++ 32_Julka.py
@@ -0,0 +1,6 @@
+# Problem Statement Link : https://www.spoj.com/problems/JULKA/
+for _ in range(10):
+	together = int(input())
+	Klaudia = int(input())
+	print((together + Klaudia) // 2)
+	print((together - Klaudia) // 2)
